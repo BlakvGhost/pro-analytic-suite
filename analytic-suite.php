b1312063fdb2817b49209714a3b58cc2636f8cff
Kabirou ALASSANEm <kabirou2001@gmail.com>
2026-05-15 15:45:21 +0100

    Bump version to 0.1.1 and update version constant

--- analytic-suite.php
+++ analytic-suite.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pro Analytics Suite
  * Plugin URI: https://github.com/BlakvGhost/Pro-Analytics-Suite
  * Description: Tableau de bord analytique pour WooCommerce et FluentBooking.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: Kabirou ALASSANE
  * Text Domain: analytic-suite
  * Domain Path: /languages
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'ANALYTIC_SUITE_VERSION', '0.1.0' );
+define( 'ANALYTIC_SUITE_VERSION', '0.1.1' );
 define( 'ANALYTIC_SUITE_FILE', __FILE__ );
 define( 'ANALYTIC_SUITE_PATH', plugin_dir_path( __FILE__ ) );
 define( 'ANALYTIC_SUITE_URL', plugin_dir_url( __FILE__ ) );
