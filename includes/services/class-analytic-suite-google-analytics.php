1eaab5ecb98767aa61979c1ec2d07fce47416d06
Kabirou ALASSANEm <kabirou2001@gmail.com>
2026-06-09 21:19:03 +0100

    Fix duration formatting by ensuring seconds are cast to integer

--- includes/services/class-analytic-suite-google-analytics.php
+++ includes/services/class-analytic-suite-google-analytics.php
@@ -729,8 +729,9 @@ class Analytic_Suite_Google_Analytics {
      * @return string
      */
     private function format_duration( $seconds ) {
+        $seconds = (int) $seconds;
         $minutes = floor( $seconds / 60 );
-        $secs    = floor( $seconds % 60 );
+        $secs    = $seconds % 60;
 
         if ( $minutes > 0 ) {
             return $minutes . 'm ' . $secs . 's';
